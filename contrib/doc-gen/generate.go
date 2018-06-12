8572871a29595f16f988d4d8d1dd1397467934f0
Karel Bilek <kb@karelbilek.com>
2018-06-12 13:57:39 +0200

    Cleanup RPC doc generating script

--- contrib/doc-gen/generate.go
+++ contrib/doc-gen/generate.go
@@ -4,23 +4,22 @@
 // (1) install golang
 // (2) install bitcoin core, set it up to use regtest
 // (3) run bitcoind
-// (4) change BITCOIN_PATH to actual bitcoin-cli directory path, change VERSION
-// (5) run this script with `go run generate.go` while being in contrib/doc-gen
-// (6) add the generated files to git
+// (4) run this script with `go run generate.go` while being in contrib/doc-gen, and with bitcoin-cli in PATH
+// (5) add the generated files to git
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"text/template"
 	"io"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
+	"text/template"
 )
 
-const BITCOIN_PATH = "/home/g/dev/bitcoind/bitcoin-0.16.0/bin/bitcoin-cli"
-const VERSION = "0.16.0"
+const BITCOIN_COMMAND = "bitcoin-cli"
 
 type Command struct {
 	Name        string
@@ -41,7 +40,23 @@ type CommandData struct {
 	Permalink   string
 }
 
+func getVersion() string {
+	allInfo := run("getnetworkinfo")
+	var f interface{}
+	err := json.Unmarshal([]byte(allInfo), &f)
+	if err != nil {
+		panic("Cannot read network info as JSON")
+	}
+	m := f.(map[string]interface{})
+
+	numv := int(m["version"].(float64))
+	v := fmt.Sprintf("%d.%d.%d", numv/1000000, (numv/10000)%100, (numv/100)%100)
+	return v
+}
+
 func main() {
+	version := getVersion()
+
 	first := run("help")
 	split := strings.Split(first, "\n")
 
@@ -85,7 +100,7 @@ func main() {
 
 	for _, group := range groups {
 		groupname := group.Name
-		dirname := fmt.Sprintf("../../_doc/en/%s/rpc/%s/", VERSION, groupname)
+		dirname := fmt.Sprintf("../../_doc/en/%s/rpc/%s/", version, groupname)
 		err := os.MkdirAll(dirname, 0777)
 		if err != nil {
 			log.Fatalf("Cannot make directory %s: %s", dirname, err.Error())
@@ -93,9 +108,9 @@ func main() {
 		for _, command := range group.Commands {
 			name := command.Name
 			address := fmt.Sprintf("%s%s.html", dirname, name)
-			permalink := fmt.Sprintf("en/doc/%s/rpc/%s/%s/", VERSION, groupname, name)
+			permalink := fmt.Sprintf("en/doc/%s/rpc/%s/%s/", version, groupname, name)
 			err = tmpl.Execute(open(address), CommandData{
-				Version:     VERSION,
+				Version:     version,
 				Name:        name,
 				Description: command.Description,
 				Group:       groupname,
@@ -105,10 +120,10 @@ func main() {
 				log.Fatalf("Cannot make command file %s: %s", name, err.Error())
 			}
 		}
-		address := fmt.Sprintf("../../_doc/en/%s/rpc/index.html", VERSION)
-		permalink := fmt.Sprintf("en/doc/%s/rpc/", VERSION)
+		address := fmt.Sprintf("../../_doc/en/%s/rpc/index.html", version)
+		permalink := fmt.Sprintf("en/doc/%s/rpc/", version)
 		err = tmpl.Execute(open(address), CommandData{
-			Version:     VERSION,
+			Version:     version,
 			Name:        "rpcindex",
 			Description: "",
 			Group:       "index",
@@ -118,10 +133,10 @@ func main() {
 			log.Fatalf("Cannot make index file: %s", err.Error())
 		}
 
-		address = fmt.Sprintf("../../_doc/en/%s/index.html", VERSION)
-		permalink = fmt.Sprintf("en/doc/%s/", VERSION)
+		address = fmt.Sprintf("../../_doc/en/%s/index.html", version)
+		permalink = fmt.Sprintf("en/doc/%s/", version)
 		err = tmpl.Execute(open(address), CommandData{
-			Version:     VERSION,
+			Version:     version,
 			Name:        "index",
 			Description: "",
 			Group:       "index",
@@ -143,7 +158,7 @@ func open(path string) io.Writer {
 }
 
 func run(args ...string) string {
-	out, err := exec.Command(BITCOIN_PATH, args...).CombinedOutput()
+	out, err := exec.Command(BITCOIN_COMMAND, args...).CombinedOutput()
 	if err != nil {
 		log.Fatalf("Cannot run bitcoin-cli: %s, is bitcoind running?", err.Error())
 	}
